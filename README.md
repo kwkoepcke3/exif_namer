279780b242a464e5943700cdb4b23ea2bbccee4a
MeiyouMayo <k3nnethwkoepcke@gmail.com>
2025-06-20 08:29:29 -0500

    add more error checks, error-quit, update README, refactor

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 install requirements.txt
 
 ## Usage
-exif_renamer.py file_with_exif.ext \[--output-dir output_dir\]
+exif_namer.py file_with_exif.ext \[--output-dir output_dir\]
 
 copies file_with_exif.ext to the same directory as the input (unless --output-dir is specified)
 with a new name, the new name being suffixed with the exif date taken data
@@ -14,12 +14,12 @@ if the --output-dir is specified, copy to that directory
 suffix: {year}-{month}-{day}_{hour}-{minute}-{second}{ext}
 
 ```sh
-usage: ExifNamer [-h] [-o OUTPUT_DIR] [--directory] [--test] input_file
+usage: exif_namer [-h] [-o OUTPUT_DIR] [--directory] [--test] input_file
 ```
 
 example: 
 ```sh
-./exif_renamer.py $HOME/unnamed.jpg
+./exif_namer.py $HOME/unnamed.jpg
 /home/mayo/unnamed_2010-11-24_18-28-47.jpg
 ```
 if --directory is given then the input is a directory, and all files in the directory/* will be copied
--- exif_namer.py
+++ exif_namer.py
@@ -1,77 +1,166 @@
 #!/usr/bin/env python
-from PIL import Image, ExifTags
+from PIL import Image, ExifTags, ImageFile
 import os
 import shutil
 import argparse
+from dataclasses import dataclass
+import sys
 
 DATE_TIME_TAKEN_K = 36867
 
 # remember to lowercase the extension
-VALID_EXTS = [".3gp", ".jpg", ".jpeg", ".png", ".gif"]
+VALID_EXTS = [".3gp", ".jpg", ".jpeg", ".png", ".gif", ".mp4", ".mov", ".avi"]
 
-def exif_rename(input_file, output_dir=None, test=False):
+
+@dataclass
+class ExifDate:
+    year: str
+    month: str
+    day: str
+
+
+@dataclass
+class ExifTime:
+    hour: str
+    minute: str
+    second: str
+
+
+@dataclass
+class ExifData:
+    date: ExifDate
+    time: ExifTime
+
+
+@dataclass
+class Args:
+    input: str
+    output_dir: str
+    directory: bool
+    dry_run: bool
+    error_quit: bool
+
+
+def extract_exif_data(input_file: str, error_quit=False) -> ExifData:
     img = Image.open(input_file)
     exif_data = img._getexif()
     if DATE_TIME_TAKEN_K not in exif_data:
         print(f"ERROR! {input_file} DOES NOT HAVE EXIF DATA! IGNORING")
+
+        exit_on_error(error_quit)
         return
     name_raw = exif_data[DATE_TIME_TAKEN_K]
 
-    ext = os.path.splitext(input_file)[1]
     date, time = name_raw.split()
     year, month, day = date.split(":")
     hour, minute, second = time.split(":")
 
+    return ExifData(ExifDate(year, month, day), ExifTime(hour, minute, second))
+
+
+def exif_rename(input_file, output_dir=None, dry_run=False, error_quit=False):
+    ext = os.path.splitext(input_file)[1]
+    if ext.lower() not in VALID_EXTS:
+        print(f"ERROR! {input_file} DOES NOT HAVE A VALID EXTENSION ({ext})")
+
+        exit_on_error(error_quit)
+        return
+
     input_name_ext = os.path.basename(input_file)
     input_name = os.path.splitext(input_name_ext)[0]
-    new_name_suffix = f"{year}-{month}-{day}_{hour}-{minute}-{second}{ext}"
+    exif: ExifData = extract_exif_data(input_file, error_quit)
+    new_name_suffix = f"{exif.date.year}-{exif.date.month}-{exif.date.day}_{exif.time.hour}-{exif.time.minute}-{exif.time.second}{ext}"
 
     new_name = f"{input_name}_{new_name_suffix}"
 
     if output_dir:
-        if os.path.exists(f"{output_dir}/{new_name}"):
-            print(f"ERROR! {output_dir}/{new_name} ALREADY EXISTS! WILL NOT REPLACE!")
-            return
-        if test:
-            print(f"COPY FROM {input_file} TO {output_dir}/{new_name}")
-            return
-
-        print(f"{output_dir}/{new_name}")
-        shutil.copy2(input_file, f"{output_dir}/{new_name}")
+        copy_from = input_file
+        copy_to = f"{output_dir}/{new_name}"
     else:
-        if os.path.exists(f"{os.path.dirname(input_file)}/{new_name}"):
-            print(f"ERROR! {output_dir}/{new_name} ALREADY EXISTS! WILL NOT REPLACE!")
-        if test:
-            print(f"COPY FROM {input_file} TO {os.path.dirname(input_file)}/{new_name}")
-            return
+        copy_from = input_file
+        copy_to = f"{os.path.dirname(input_file)}/{new_name}"
 
-        print(f"{os.path.dirname(input_file)}/{new_name}")
-        shutil.copy2(input_file, f"{os.path.dirname(input_file)}/{new_name}")
-    
+    if os.path.exists(copy_to):
+        print(f"ERROR! {copy_to} ALREADY EXISTS! WILL NOT REPLACE!")
+
+        exit_on_error(error_quit)
+        return
+
+    print(f"COPY FROM {copy_from} TO {copy_to}")
+    if not dry_run:
+        shutil.copy2(copy_from, copy_to)
+
+
+def exit_on_error(error_quit=False):
+    if error_quit:
+        print("EXITING...")
+        sys.exit(-1)
+    else:
+        print("SKIPPING!")
+
 
 def main():
-    parser = argparse.ArgumentParser(prog="ExifNamer", description="Copies file to destination with date taken as name")
-    parser.add_argument("input_file")
-    parser.add_argument('-o', "--output-dir", default=None)
-    parser.add_argument('--directory', action='store_true')
-    parser.add_argument('--test', action='store_true')
+    parser = argparse.ArgumentParser(
+        prog="exif_namer",
+        description="Copies image file with date taken as name suffix",
+        epilog=f"""
+Copies image file with date taken as name suffix
 
-    args = parser.parse_args()
-    
+Does not copy if image file has unknown extension! ({VALID_EXTS})
+Does not copy if image file destination already exists!
+Does not copy if image file does not have appropriate exif data!
+Does not copy if in dry_run mode!
+""",
+    )
+    parser.add_argument(
+        "input", help="The input file or directory to copy image file(s) from"
+    )
+    parser.add_argument(
+        "-o",
+        "--output-dir",
+        default=None,
+        help="If set, output copies to this directory",
+    )
+    parser.add_argument(
+        "-d",
+        "--directory",
+        action="store_true",
+        help="If set, the input is a directory",
+    )
+    parser.add_argument(
+        "-t",
+        "--dry-run",
+        action="store_true",
+        help="If set, do not copy, only show log",
+    )
+    parser.add_argument(
+        "-e",
+        "--error-quit",
+        action="store_true",
+        help="If set, quit on error. By default in directory mode will not quit if encountering an error, and will continue to the next file if possible",
+    )
+
+    args: Args = parser.parse_args()
 
     if args.directory:
-        # args.input_file will be a directory if --directory is specified
-        for file in os.listdir(args.input_file):
-            ext = os.path.splitext(file)[1].lower()
-            if ext not in VALID_EXTS:
-                print(f"ERROR! {args.input_file}/{file} DOES NOT HAVE A VALID EXTENSION ({ext})")
-                continue
-            
-            exif_rename(f"{args.input_file}/{file}", args.output_dir, args.test)
+        if not os.path.isdir(args.input):
+            print(
+                f"ERROR! {args.input} IS NOT A VALID DIRECTORY! DO NOT KNOW WHERE TO COPY TO!"
+            )
+            exit_on_error()
+
+        # args.input will be a directory if --directory is specified
+        for file in os.listdir(args.input):
+            exif_rename(
+                f"{args.input}/{file}", args.output_dir, args.dry_run, args.error_quit
+            )
     else:
-        exif_rename(args.input_file, args.output_dir, args.test)
-                
-    
+        if not os.path.isfile(args.input):
+            print(f"ERROR! {args.input} IS NOT A VALID FILE! CANNOT COPY!")
+            exit_on_error()
+
+        exif_rename(args.input, args.output_dir, args.dry_run, args.error_quit)
+
+
 if __name__ == "__main__":
     main()
-
