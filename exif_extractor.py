818b82d1959812fa867c1840d4e237baeca47fa5
MeiyouMayo <k3nnethwkoepcke@gmail.com>
2025-06-20 08:29:59 -0500

    refactor

--- exif_extractor.py
+++ exif_extractor.py
@@ -6,13 +6,17 @@ import argparse
 
 DATE_TIME_TAKEN_K = 36867
 
+
 def main():
-    parser = argparse.ArgumentParser(prog="ExifNamer", description="Copies file to destination with date taken as name")
+    parser = argparse.ArgumentParser(
+        prog="ExifNamer",
+        description="Copies file to destination with date taken as name",
+    )
     parser.add_argument("input_file")
-    parser.add_argument('-o', "--output-dir", default=None)
+    parser.add_argument("-o", "--output-dir", default=None)
 
     args = parser.parse_args()
-    
+
     img = Image.open(args.input_file)
     exif_data = img._getexif()
     name_raw = exif_data[DATE_TIME_TAKEN_K]
@@ -20,12 +24,11 @@ def main():
     for key, val in exif_data.items():
         if key in ExifTags.TAGS:
             print("Known Tag")
-            print(f'{key}:{ExifTags.TAGS[key]}:{val}')
+            print(f"{key}:{ExifTags.TAGS[key]}:{val}")
         else:
             print("Unknown Tag")
-            print(f'{key}:{val}')
-   
-    
+            print(f"{key}:{val}")
+
+
 if __name__ == "__main__":
     main()
-
