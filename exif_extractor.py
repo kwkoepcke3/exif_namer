c16cd42aba712bcee77dc79907c82e42863c2470
Meiyou Mayo <meiyoumayo@MacBookPro.attlocal.net>
2025-06-23 07:09:29 -0500

    remove exif index in extractor

--- exif_extractor.py
+++ exif_extractor.py
@@ -4,9 +4,6 @@ import os
 import shutil
 import argparse
 
-DATE_TIME_TAKEN_K = 36867
-
-
 def main():
     parser = argparse.ArgumentParser(
         prog="ExifNamer",
@@ -19,7 +16,6 @@ def main():
 
     img = Image.open(args.input_file)
     exif_data = img._getexif()
-    name_raw = exif_data[DATE_TIME_TAKEN_K]
 
     for key, val in exif_data.items():
         if key in ExifTags.TAGS:
